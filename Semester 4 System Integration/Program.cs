4e93f0dff183f60a6c415ab897cc8161c6c554ef
BuildTools <unconfigured@null.spigotmc.org>
2024-02-06 10:00:01 +0100

    Add project files.

---
+++ Semester 4 System Integration/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lek2_Opgave2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
